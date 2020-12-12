27ba914b354acdec2dcff8d5bee71eaffd2e5e50
hinaba3 <hinaba3@DESKTOP-OMOFMOM.localdomain>
2020-12-12 15:19:17 +0900

    add WS file transfer function for client/server

--- servey/ws-test/ws_client.js
+++ servey/ws-test/ws_client.js
@@ -1,7 +1,16 @@
+const encodeType = 'binary';
+const filename = 'banzai_boy.png';
+
 var WebSocket = require('ws');
 var ws = new WebSocket('ws://127.0.0.1:8080');
 ws.on('open', function() {
   var now = new Date();
-  ws.send('hello, ' + now);
+
+  const fs = require('fs');
+  const buff = fs.readFileSync(filename, encodeType);
+  console.log(buff);
+  console.log('Send: ' + buff.length);
+
+  ws.send(buff);
   ws.close();
 });
--- servey/ws-test/ws_server.js
+++ servey/ws-test/ws_server.js
@@ -1,12 +1,15 @@
+const decodeType = 'binary';
+
 const server = require("ws").Server;
 const s = new server({ port: 8080 });
+var fs = require('fs');
 
 s.on("connection", ws => {
   ws.on("message", message => {
-    console.log("Received: " + message);
-
-    if (message === "hello") {
-      ws.send("hello from server");
-    }
+    console.log("Received: " + message.length);
+    var decode = Buffer.from(message, decodeType);
+    fs.writeFile('xxx.png', decode, function(err) {
+      console.log(err);
+    });
   });
 });
